c31c185dd60af21ce4a6c00ab144730d3341cdbd
Min Huang <h.min719@gmail.com>
2020-06-17 17:05:38 +0200

    [Cards] revise badge css

--- src/components/common/Cards/Cards.jsx
+++ src/components/common/Cards/Cards.jsx
@@ -1,45 +1,35 @@
 import React from 'react';
 import Countup from 'react-countup';
-import cx from 'classnames';
 import styles from './Cards.module.css';
+import { color } from '../Chart/chartConfig';
 
 const Cards = ({
   data: { cases, todayCases, recovered, todayRecovered, deaths, todayDeaths },
 }) => {
   if (!cases) return null;
+  const setBackgroundColor = (title) => { 
+    const type = title.toLocaleLowerCase();
+    return { background: color[type] };
+  };
 
-  const contents = [{
-    title: 'Confirmed',
-    style: styles.confirmed,
-    value: cases,
-    todayValue: todayCases,
-  }, {
-    title: 'Recovered',
-    style: styles.recovered,
-    value: recovered,
-    todayValue: todayRecovered,
-  }, {
-    title: 'Deaths',
-    style: styles.deaths,
-    value: deaths,
-    todayValue: todayDeaths,
-  }];
+  const contents = [
+    { title: 'Confirmed', value: cases, todayValue: todayCases }, 
+    { title: 'Recovered', value: recovered, todayValue: todayRecovered }, 
+    { title: 'Deaths', value: deaths, todayValue: todayDeaths }
+  ];
   
   return (
     <div className={styles.container}>
-      {contents.map(({ style, title, value, todayValue }) => (
+      {contents.map(({ title, value, todayValue }) => (
         <div key={title} className={styles.card}>
-          <div className={cx(styles.indicator, style)} />
+          <div className={styles.indicator} style={setBackgroundColor(title)} />
           <p className={styles.title}>{title}</p>
 
-          <div className={styles.text}>
-            <p className={styles.value}>
-              <Countup start={0} end={value} duration={1} separator=',' />
-            </p>
-
+          <div className={styles.value}>
+            <Countup start={0} end={value} duration={1} separator=',' />
             {todayValue !== 0 && (
-              <span className={cx(style, styles.badge)}>
-                + {todayValue.toLocaleString()}
+              <span className={styles.badge} style={setBackgroundColor(title)}>
+                +{todayValue.toLocaleString()}
               </span>
             )}
           </div>
