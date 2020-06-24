e5d1986f2b58d1771a80666fd1df5ffeac1c6217
Min Huang <h.min719@gmail.com>
2020-06-24 15:58:19 +0200

    [App] footer rwd

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from 'react';
 import { fetchCountries, fetchHistoryData } from './api';
 import { Navbar, Total, History } from './components';
 import styles from './App.module.css';
+import IconButton from '@material-ui/core/IconButton';
+import MailOutlineSharpIcon from '@material-ui/icons/MailOutlineSharp';
 
 const App = () => {
   const [countries, setCountries] = useState([]);
@@ -30,15 +32,16 @@ const App = () => {
       </main>
 
       <footer>
-        <div>
-          <p>{`[ API ] `}
-            <a href={link} target='_blank' rel='noopener noreferrer'>
-              NOVELCovid
-            </a>
-          </p>
-          <p>[ DATA ] Worldometers, Johns Hopkins University</p>
-        </div>
+        <p className={styles.api}>
+          {`[ Data API ] `}
+          <a href={link} target='_blank' rel='noopener noreferrer'>NOVELCovid</a>
+        </p>
         <p className={styles.text}>Copyright © 2020 Min Huang</p>
+        <a href='mailto:h.min719@gmail.com' className={styles.mail}>
+          <IconButton className={styles.icon}>
+            <MailOutlineSharpIcon />
+          </IconButton>
+        </a>
       </footer>
     </>
   );
--- src/components/common/AnchoredTitle/AnchoredTitle.jsx
+++ src/components/common/AnchoredTitle/AnchoredTitle.jsx
@@ -3,14 +3,28 @@ import Anchor from './Anchor';
 import styles from './AnchoredTitle.module.css';
 import cx from 'classnames';
 
-export const AnchoredTitle = ({ hrefId, classes, color = 'rgba(255, 255, 255, 0.87)', children }) => (
-  <a href={`#${hrefId}`} className={cx(classes, styles.title)} style={{ color, marginBottom: 24 }}>
+export const AnchoredTitle = ({
+  hrefId,
+  classes,
+  color = 'rgba(255, 255, 255, 0.87)',
+  children,
+}) => (
+  <a
+    href={`#${hrefId}`}
+    className={cx(classes, styles.title)}
+    style={{ color, marginBottom: 24 }}
+  >
     <Anchor color={color} />
     <h1>{children}</h1>
   </a>
 );
 
-export const AnchoredSubtitle = ({ hrefId, color = 'rgba(255, 255, 255, 0.87)', title, subtitle }) => (
+export const AnchoredSubtitle = ({
+  hrefId,
+  color = 'rgba(255, 255, 255, 0.87)',
+  title,
+  subtitle,
+}) => (
   <>
     <a href={`#${hrefId}`} className={styles.title} style={{ color }}>
       <Anchor color={color} size={12} />
